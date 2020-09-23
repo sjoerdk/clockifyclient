818e6ec8bc7ecc7701ca26e4e2ffba05acda0659
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-09-23 10:44:06 +0200

    Bump version: 0.1.11 → 0.2.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sjoerdk/clockify_api_client",
-    version="0.1.11",
+    version="0.2.0",
     zip_safe=False,
 )
